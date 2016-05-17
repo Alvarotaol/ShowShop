ee92f637edcd7d255a6e6cdbc4f41b3b99bda278
arubaro <ato.cdf@gmail.com>
2016-05-16 21:48:25 -0300

    Campo para usuário fornecedor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lojas
-  devise_for :usuarios
+  devise_for :usuarios, :controllers => { registrations: 'registrations' }
   resources :produtos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
