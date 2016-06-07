89ff00ef542bf6d2b94897c94ed16dc922b65e12
victorsv <victor.psr@gmail.com>
2016-06-07 09:58:32 -0300

    Layout modificado

--- app/models/loja.rb
+++ app/models/loja.rb
@@ -2,7 +2,7 @@ class Loja < ActiveRecord::Base
   has_many :produtos, dependent: :destroy
     has_many :categoriums, dependent: :destroy
     has_many :info_lojas, dependent: :destroy
-    has_attached_file :imagem, :styles => { :medium => "300x300>", :thumb => "50x50#" }
+    has_attached_file :imagem, :styles => { :medium => "300x300>", :thumb => "150x150#" }
     validates :nome, presence: true
     validates :cnpj, presence: true, uniqueness: true
     validates_attachment_content_type :imagem, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,5 @@
 class Produto < ActiveRecord::Base
-  has_attached_file :imagem, :styles => { :medium => "300x300>", :thumb => "50x50#" }
+  has_attached_file :imagem, :styles => { :medium => "350x350>", :thumb => "250x150#", :front => "800x300",  }
   validates_attachment_content_type :imagem, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   #validates_attachment_presence :imagem
   belongs_to :loja
